3836f1aca5b86d3e8f68916486ac0576cbbe9d99
GhedeoNts <contact.ntsiassila@gmail.com>
2023-01-23 19:29:50 +0100

    First initialization of the project and installation of the necessary dependencies!

---
+++ config-example/default.ts
@@ -0,0 +1,14 @@
+import pg from 'pg';
+
+export default {
+    saltWorkFactor : ,
+    // Database information
+    port : ,
+    dataBaseHost: '',
+    dataBaseName : '',
+    dataBaseUser : '',
+    dataBasePassword: '',
+    dataBasePort : ,
+    dataBaseDriver: '',
+    dataBaseDialectModule: pg,
+}
